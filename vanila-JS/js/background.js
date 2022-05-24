6eb3d12fc54a49b3dc2b281f5f14ed1368d523a5
WKlee0607 <fovert@khu.ac.kr>
2022-05-25 02:11:47 +0900

    Update background.js

--- vanila-JS/js/background.js
+++ vanila-JS/js/background.js
@@ -1,4 +1,4 @@
-const body = document.querySelector("body");
+/*const body = document.querySelector("body");
 const biUrls = [
     "https://images.unsplash.com/photo-1433838552652-f9a46b332c40?crop=entropy&cs=tinysrgb&fm=jpg&ixid=Mnw0MDI0M3wwfDF8cmFuZG9tfHx8fHx8fHx8MTY1MzI0MDAwOA&ixlib=rb-1.2.1&q=80"
 ];
@@ -9,7 +9,7 @@ function randomChooseBackgroundImage(){
     body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${chosenImage})`
 }
 
-randomChooseBackgroundImage();
+randomChooseBackgroundImage();*/
 
 
 
@@ -19,8 +19,10 @@ randomChooseBackgroundImage();
 
 
 const images = [
-    "0.jpeg",
-    "1.jpeg"
+    "1.jpeg",
+    "2.jpeg",
+    "3.jpeg",
+    "4.jpeg"
 ];
 
 const chosenImage = images[Math.floor(Math.random()*images.length)];
@@ -30,5 +32,5 @@ const bgIamge = document.createElement("img");
 bgIamge.src = `img/${chosenImage}`;
 
 const username = document.querySelector("#greeting");
-document.body.appendChild(bgIamge);
+document.body.prepend(bgIamge);
 
