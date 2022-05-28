b940a3de07bfe4b20e295e390385572a8f6c4e96
WKlee0607 <fovert@khu.ac.kr>
2022-05-28 11:03:37 +0900

    how to make ResetBtn & 듣던부분부터 재생하는 코드
    제목 그래도 듣던부분부터 재생하는 코드랑, 처음부터 재생하는 코드 만듦

--- vanila-JS/js/audio.js
+++ vanila-JS/js/audio.js
@@ -48,3 +48,68 @@ function handleNextButtinClick(){
 playBtn.addEventListener("click",loadAudio);
 stopBtn.addEventListener("click",stopAudio);
 nextBtn.addEventListener("click",handleNextButtinClick);
+
+/*
+const urls = [
+    "말리지마-(여자)아이들",
+    "mybag-(여자)아이들",
+    "TOMBOY-(여자)아이들"
+];
+const playBtn = document.createElement("button");
+playBtn.innerText = "play";
+const nextBtn = document.createElement("button");
+nextBtn.innerText = "next";
+const stopBtn = document.createElement("button");
+stopBtn.innerText = "stop";
+const resetBtn = document.createElement("button");
+resetBtn.innerText = "처음부터";
+const audioSection = document.querySelector(".audio-section");
+audioSection.appendChild(playBtn);
+audioSection.appendChild(nextBtn);
+audioSection.appendChild(stopBtn);
+audioSection.appendChild(resetBtn);
+
+const urlAll = (urls.length -1);
+let currentUrl = 0;
+
+let audio = new Audio();
+    
+let current = audio.currentTime;
+
+function playAudio(){
+    audio.src = `audio/${urls[currentUrl]}.mp3`
+    audio.load();
+    audio.volume = 0.5
+    audio.currentTime = current;
+    audio.play();
+}
+
+function nextCurrenturl(){
+    if(currentUrl < urlAll){
+        currentUrl += 1;
+    }else{
+        currentUrl = 0;
+    }
+    audio.pause();
+    current = 0;
+    playAudio();
+}
+
+function pauseAudio(){
+    audio.pause();
+    current = audio.currentTime;
+}
+
+function resetCurrent(){
+    current = 0;
+    playAudio();
+}
+
+
+
+stopBtn.addEventListener("click",pauseAudio);
+playBtn.addEventListener("click",playAudio);
+nextBtn.addEventListener("click",nextCurrenturl);
+resetBtn.addEventListener("click",resetCurrent);
+
+*/
