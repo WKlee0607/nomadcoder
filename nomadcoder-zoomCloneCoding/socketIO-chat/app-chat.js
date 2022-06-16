adcc45b697afe5c4d479a4562025c5125a64e9e2
WKlee0607 <fovert@khu.ac.kr>
2022-06-17 04:41:36 +0900

    socketIO-chat
    socketIO로 만든 chating방은 이해를 socketIO 이해를 돕기 위해 한 강의였어서, 본래 src에서는 지워주고 따로 보관

---
+++ nomadcoder-zoomCloneCoding/socketIO-chat/app-chat.js
@@ -0,0 +1,83 @@
+/*const frontSocket = io();//backend Socket과 연결됨.
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+const room = document.getElementById("room");
+const roomMsgForm = room.querySelector("#msg");
+const roomNameForm = room.querySelector("#name");
+
+roomMsgForm.hidden = true;
+
+let roomName;
+
+function addMessage(message){
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerText = message;
+    ul.appendChild(li);
+};
+
+function handleMessageSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#msg input");
+    const value = input.value;
+    frontSocket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You: ${value}`);
+    });
+    input.value = "";
+};
+
+function handleNicknameSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#name input");
+    frontSocket.emit("nickname", input.value);
+};
+
+function showRoom(){
+    welcome.hidden = true;
+    roomNameForm.hidden = true;
+    roomMsgForm.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room: ${roomName}`;
+    roomMsgForm.addEventListener("submit",handleMessageSubmit);
+};
+
+function handleRoomSubmit(event){
+    event.preventDefault();
+    const input = form.querySelector("input");
+    frontSocket.emit("enter_room", input.value, showRoom);//Websocket(WS)에서의 send와 같은 역할인 거 같음. WS에서는 object를 string형태로 보냈어야 했는데, SocketIO에서는 그럴 필요없이 바로 object로 보낼 수 있음, 또한 3번쨰 arg로 callback fn이 들어가는데 이 fn도 서버(backend)에서 인수로 받아 서버에서 제어 가능  물론 실행은 frontend에서 함!
+    roomName = input.value;
+    input.value = "";
+};
+
+form.addEventListener("submit",handleRoomSubmit);
+roomNameForm.addEventListener("submit",handleNicknameSubmit);
+
+frontSocket.on("welcome",(user, newCount) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room: ${roomName} (${newCount})`;
+    addMessage(`${user} joined!`);
+});
+
+frontSocket.on("bye",(left, newCount) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room: ${roomName} (${newCount})`;
+    addMessage(`${left} left ㅠㅠ`);
+});
+
+frontSocket.on("new_message", (msg) => {
+    addMessage(msg);
+});
+
+frontSocket.on("room_change", (publicRooms) => {
+    const roomList = welcome.querySelector("ul");
+    roomList.innerHTML = "";
+    if(publicRooms.length === 0){
+        return;
+    };
+    publicRooms.forEach((room) => {
+        const li = document.createElement("li");
+        li.innerText = room;
+        roomList.append(li);
+    });
+});*/
---
+++ nomadcoder-zoomCloneCoding/socketIO-chat/server-chat.js
@@ -0,0 +1,72 @@
+/*import http from "http";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
+import express from "express";
+
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views"); // 현재 실행중인 폴더 경로 = process.cwd()와 같다고 볼 수 있음. __dirname : returns the directory name of the directory containing the JavaScript source code file// process.cwd(): returns the current working directory, 즉, node명령을 호출한 디렉토리입니다.
+app.use("/public", express.static(__dirname + "/public"));//이미지, CSS 파일 및 JavaScript 파일과 같은 정적 파일을 제공하려면 Express의 기본 제공 미들웨어 함수인 express.static을 사용하십시오. -> app.use('/static', express.static(__dirname + '/public')); 이제 /static 경로 접두부를 통해 public 디렉토리에 포함된 파일을 로드할 수 있습니다.// 정적 파일이란, 직접 값에 변화를 주지 않는 이상 변하지 않는 파일을 의미합니다. 예를 들면, image, css 파일, js 파일 등을 의미합니다. -> static: 정적 파일만을 제공////보안상 유저는 서버 내 모든 폴더를 전부 들여다볼 수 없음(보안상의 이유 때문에) 그래서 유저가 볼 수 있는 폴더를 따로 지정해야댐. 즉, 이건 Front-end폴더임.
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));// -> catchall : 어떤 url을 가든 "/"로 돌아오게 만들어 home.pug만 볼 수 있도록 하기. 이번 프로젝트에서는 하나의 url만 사용할 것이기 때문에 이렇게 처리해줌.
+
+const httpServer = http.createServer(app);//http 서버 만들기.
+const ioServer = new Server(httpServer, {
+    cors: {
+      origin: ["https://admin.socket.io"],
+      credentials: true
+    },
+  });//SocketIO로 서버를 만든 것임. 이전처럼 http위에 서버를 덮어썼지만.
+
+instrument(ioServer, {
+    auth: false
+});
+
+function publicRooms(){
+    const {sockets : {adapter: {sids, rooms}}} = ioServer;
+    // 위의 콘드는 밑의 두 줄을 풀어쓴 거. 	
+	//const sids = ioServer.sockets.adapter.sids; 
+    //const rooms = ioServer.sockets.adapter.rooms;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if(sids.get(key) === undefined){
+            publicRooms.push(key);
+        }
+    });
+    return publicRooms;
+};// rooms에서 sids(개인방)을 빼주는 작업임.
+
+function countRoom(roomName){
+    return ioServer.sockets.adapter.rooms.get(roomName)?.size
+};
+
+ioServer.on("connection",(backSocket) => {
+    backSocket["nickname"] = "Anonymous";
+    backSocket.onAny((event) => {
+        console.log(ioServer.sockets.adapter);
+        console.log(`Socket Event: ${event}`);
+    });
+    backSocket.on("enter_room", (roomName, done) => {
+        backSocket.join(roomName);
+        done();
+        backSocket.to(roomName).emit("welcome", backSocket.nickname, countRoom(roomName));
+        ioServer.sockets.emit("room_change", publicRooms());
+    });
+    backSocket.on("disconnecting",() => {
+        backSocket.rooms.forEach((room) => backSocket.to(room).emit("bye", backSocket.nickname, countRoom(room) -1 ));//backSocket.rooms: {"id~~", "roomName"}
+    });
+    backSocket.on("disconnect", () => {
+        ioServer.sockets.emit("room_change", publicRooms());
+    });
+    backSocket.on("new_message",(msg, room, done) => {
+        backSocket.to(room).emit("new_message", `${backSocket.nickname}: ${msg}`);
+        done();
+    });
+    backSocket.on("nickname",(nickname) => backSocket["nickname"] = nickname);
+});
+
+
+const handleListen = () => console.log(`Listening on http://localhost:3000, ws://localhost:3000`);
+httpServer.listen(3000,handleListen);*/
