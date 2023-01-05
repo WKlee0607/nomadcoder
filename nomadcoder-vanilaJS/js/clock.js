144cb373872f01b086ed398a60b81fe497fb2152
WKlee0607 <fovert@khu.ac.kr>
2023-01-06 02:32:35 +0900

    modify getMonth, getDay

--- nomadcoder-vanilaJS/js/clock.js
+++ nomadcoder-vanilaJS/js/clock.js
@@ -1,9 +1,9 @@
-const clock = document.querySelector("h2#clock");
+const clock = document.querySelector("#clock");
 
 function getClock(){
     const date = new Date();
-    const months = String(date.getMonth());
-    const days = String(date.getDay())
+    const months = String(date.getMonth()+1); // getMonth는 0~11까지만 return so, +1
+    const days = String(date.getDay()+1);
     const hours = String(date.getHours()).padStart(2,"0");
     const minutes = String(date.getMinutes()).padStart(2,"0");
     const seconds = String(date.getSeconds()).padStart(2,"0");
